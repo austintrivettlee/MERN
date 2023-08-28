384ef0935b961bc6278b526293a4d5d847f323d9
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-08-28 15:42:29 -0500

    assignment

---
+++ practise/test.js
@@ -0,0 +1,21 @@
+let x = 1;
+let y = 2;
+let z = 3;
+console.log(x + x);
+console.log(y - z);
+console.log(z * x);
+
+var beatles = ['Paul', 'George', 'John', 'Ringo'];
+function printNames(names) {
+  function actuallyPrintingNames() {
+    for (var index = 0; index < names.length; index++) {
+      var name = names[index];
+  
+      console.log(name + ' was found at index ' + index);
+    }
+    console.log('name and index after loop is ' + name + ':' + index);
+  }
+  actuallyPrintingNames();
+}
+printNames(beatles);
+
