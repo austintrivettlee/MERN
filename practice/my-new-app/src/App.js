1daedc7a049a8a3bff16542f3d4b9d3144da45b5
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-09-04 14:00:42 -0500

    propitup

--- practice/my-new-app/src/App.js
+++ practice/my-new-app/src/App.js
@@ -1,18 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import PersonCard from "./PersonCard";
 
 function App() {
   return (
-    <>
-      <h1>Hello Dojo!</h1>
-      <h3>Things I need to do:</h3>
-      <ul>
-        <li>Learn React</li>
-        <li>Climb Mt. Everest</li>
-        <li>Run a marathon</li>
-        <li>Feed the Dogs</li>
-      </ul>
-    </>
+    <div className="App">
+      <PersonCard firstName = {"Jane"} lastName = {"Doe"} age = {45} hairColor = {"Black"}/>
+      <PersonCard firstName = {"John"} lastName = {"Smith"} age = {88} hairColor = {"Brown"} />
+      <PersonCard firstName = {"Millard"} lastName = {"Fillmore"} age = {50} hairColor = {"Brown"} />
+      <PersonCard firstName = {"Maria"} lastName = {"Smith"} age = {62} hairColor = {"Brown"} />
+    </div>
   );
 }
 
