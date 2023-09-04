337271be6795c489226732ef4b257d9daa530d92
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-09-04 13:32:01 -0500

    intro app

---
+++ practice/intro.jsx
@@ -0,0 +1,9 @@
+const App = () => <h1>Our First React Page has rendered</h1>;
+
+ReactDom.render(<App/>, document.getElementById("root"));
+
+
+<script type="text/babel">
+    ReactDOM.render(<App />, document.getElementById('root'));
+</script>
+
