116d376ac7d1886520caec38ba5928d6b7d04f46
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-09-04 12:44:52 -0500

    upddate

--- practice/test.js
+++ practice/test.js
@@ -19,3 +19,23 @@ function printNames(names) {
 }
 printNames(beatles);
 
+setTimeout( function() { 
+  console.log("start") 
+}, 3000 );
+    
+console.log("end");
+
+var exampleFunction = function(message){
+  console.log( message );
+};
+    
+exampleFunction( "Hello from exampleFunction" );
+
+function parentFunction( callback ) {
+  callback( "information from the parent function" );
+}
+
+parentFunction( exampleFunction );
+
+const groceries = ["pearl onions", "cremini mushrooms", "thyme"];
+const groceryList = groceries.map( item => `<li>${item}</li>` );
