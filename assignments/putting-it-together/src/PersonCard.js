c9d905dc0d6360332e46bab854cecfcb5a2f728f
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-09-04 16:52:57 -0500

    birthday button

--- assignments/putting-it-together/src/PersonCard.js
+++ assignments/putting-it-together/src/PersonCard.js
@@ -1,16 +1,28 @@
+import { useState } from 'react'
+
 const PersonCard = (props) => {
-    const { firstName, lastName, age, hairColor} = props;
+    const { firstName, lastName, hairColor, age} = props;
+
+    const [state, setState] = useState({age: age});
+    
+    const handleClick = () => {
+        setState({
+            age: state.age + 1
+        });
+    }
+
     return (
         <div>
             <h1>
                 {lastName}, {firstName}
             </h1>
             <p>
-                Age {age}
+                Age: {state.age}
             </p>
             <p>
                 Hair Color: {hairColor}
             </p>
+            <button onClick={ handleClick }>Birthday Button for {firstName} {lastName}</button>
         </div>
     )
 }
