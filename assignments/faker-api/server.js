423d4e4521d6bdbaa41fc0c4069ec319be8060e9
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-09-16 19:39:04 -0500

    updates

---
+++ assignments/faker-api/server.js
@@ -0,0 +1,23 @@
+// The import line will look different than what is in Faker's documentation
+// because we are working with an express application
+const { faker } = require('@faker-js/faker');
+// we can create a function to return a random / fake "Product"
+const createProduct = () => {
+    const newFake = {
+        name: faker.commerce.productName(),
+        price: "$" + faker.commerce.price(),
+        department: faker.commerce.department()
+    };
+    return newFake;
+};
+    
+const newFakeProduct = createProduct();
+console.log(newFakeProduct);
+/*
+ * The output of the above console log will look like this
+ * {
+ *   name: 'Anime Figure',
+ *   price: '$568.00
+ *   department: 'Tools' 
+ * }
+ */
